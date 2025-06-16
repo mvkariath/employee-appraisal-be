c54e749aafc472a0355c685ac16e6b53b728674a
Manukrishnan P <manu8krishnan@gmail.com>
2025-06-16 09:09:55 +0530

    updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,50 @@
 import express, { Request, Response } from 'express';
+import { LoggerService } from './services/logger.service';
+import loggerMiddleware from './middlewares/loggerMiddleware';
+import { authMiddleware } from './middlewares/auth.middleware';
+import employeeRouter from './routes/employee.route';
+import { authRouter } from './routes/auth.route';
+import { errorHandlineMiddleware } from './middlewares/errorHandlingMiddleware';
+import cors from 'cors'
+import datasource from '../db/data-source';
+import appraisalCycleRouter from './routes/appraisalCycle.route';
+import appraisalRouter from './routes/appraisal.route';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+const logger = LoggerService.getInstance('app()');
+
+app.use(cors())
+app.use(express.json());
+app.use(loggerMiddleware);
+
+app.use("/employee", authMiddleware, employeeRouter);
+app.use("/appraisalCycle", authMiddleware, appraisalCycleRouter);
+app.use("/appraisal", authMiddleware, appraisalRouter);
+
+
+app.use("/auth",authRouter)
+
+
+app.use(errorHandlineMiddleware)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from TypeScript + Express!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+(async () => {
+  try{
+    await datasource.initialize();
+    logger.info("DB connected");
+  }
+  catch(error) {
+    logger.error(`Failed to connect to DB - ${error}`);
+    process.exit(1);
+  }
+  app.listen(3000, () => {
+    logger.info("server listening to 3000");
+  });
+
+})();
